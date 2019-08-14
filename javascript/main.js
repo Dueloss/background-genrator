bd275b01a3293970c4acbed96260e82eadbc9ff5
Dueloss <kim.b.johansson@gmail.com>
2019-08-14 10:50:16 +0200

    added javaScript and set background color on load

---
+++ javascript/main.js
@@ -0,0 +1,15 @@
+const colorPicker1 = document.getElementsByClassName('color')[0];
+const colorPicker2 = document.getElementsByClassName("color")[1];
+const body = document.getElementById("gradient");
+const css = document.querySelector('h3');
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  setColor();
+  colorPicker1.addEventListener("change", setColor);
+  colorPicker2.addEventListener("change", setColor);
+});
+const setColor = () => {
+  console.log("clicked");
+  body.style.background = `linear-gradient(to right, ${colorPicker1.value}, ${colorPicker2.value})`;
+  css.textContent = body.style.background;
+}
